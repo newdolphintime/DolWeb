3808048bc3ddfeade3dbbce227973d94183c3fed
zhangwei123 <1551583950@qq.com>
2017-05-02 22:45:56 +0800

    哈哈哈哈  session购物车

--- src/ServletSession1.java
+++ src/ServletSession1.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Zv on 2017/05/01.
@@ -16,10 +17,15 @@ public class ServletSession1 extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=utf-8");
         HttpSession httpSession = request.getSession();
         String id = httpSession.getId();
         System.out.println(id);
         httpSession.setAttribute("name","张威");
+
+        PrintWriter out = response.getWriter();
+        out.write("<a href = '" + request.getContextPath() + "/session2'>返回主页继续浏览</a>");
 
     }
 }
